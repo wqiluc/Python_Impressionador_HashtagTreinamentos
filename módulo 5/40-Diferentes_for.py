fe49f3fcca2f65624bd149b7dd0e8dd7bae9d80b
Lucas Paguetti Pereira <lucaspaguetti2@gmail.com>
2026-01-21 14:33:50 -0300

    módulo 4 updates

---
+++ módulo 5/40-Diferentes_for.py
@@ -0,0 +1,20 @@
+from cores import(Reset,Amarelo,Azul,Magenta)
+
+print(f"""
+{Azul}Exercício — Listas e Estruturas de Repetição 📋🔃{Reset}
+
+{Magenta}Este script trabalha com duas listas relacionadas:{Reset}
+produtos; preços.
+
+{Azul}🔁 Estruturas utilizadas: {Reset}
+
+{Amarelo}1. For item in lista: {Reset}
+Percorre diretamente os valores da lista de preços para calcular o preço com imposto.
+
+{Amarelo}2. For i in range(len(lista)): {Reset}
+Utiliza índices para acessar simultaneamente produtos e preços, exibindo o preço de cada produto.
+
+{Amarelo}3. For item in lista com enumerate: {Reset}
+Combina índice e valor para calcular e exibir o preço de cada produto com imposto de forma organizada.
+
+{Magenta}Objetivo🪓: praticar loops, acesso por índice e associação entre listas.{Reset}""")
